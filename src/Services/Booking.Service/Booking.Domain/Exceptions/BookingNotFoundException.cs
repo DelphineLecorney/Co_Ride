35464e0a843ba06e8a19b5f160fdce1d743b9499
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2026-04-25 16:25:49 +0200

    Added Commands Booking, Exceptions and interfaces.

---
+++ src/Services/Booking.Service/Booking.Domain/Exceptions/BookingNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.Domain.Exceptions
+{
+    public class BookingNotFoundException : Exception
+    {
+        public BookingNotFoundException(Guid bookingId)
+            : base($"La réservation avec l'ID {bookingId} n'a pas été trouvé.")
+        {
+        }
+    }
+}
---
+++ src/Services/Booking.Service/Booking.Domain/Exceptions/TripNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.Domain.Exceptions
+{
+    public class TripNotFoundException : Exception
+    {
+        public TripNotFoundException(Guid tripId)
+            : base($"Le trajet avec l'ID {tripId} n'a pas été trouvé.")
+        {
+        }
+    }
+}
