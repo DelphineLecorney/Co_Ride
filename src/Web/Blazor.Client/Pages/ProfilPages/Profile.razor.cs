ab510afeba4f50434f5a387e302a203b2c33165f
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2026-05-15 14:10:03 +0200

    Move the “Notification.Application” and ‘Domain’ folders to “Notification.Service”

---
+++ src/Web/Blazor.Client/Pages/ProfilPages/Profile.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Client.Pages.UserPages
+{
+    public partial class Profile : ComponentBase
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        protected async Task Create()
+        {
+            Console.WriteLine($"Création du profil : {FirstName} {LastName}");
+
+        }
+    }
+}
